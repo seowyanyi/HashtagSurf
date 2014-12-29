9a90c160b4da98477ab7d617961d9ce3ebd66271
Yan Yi <seow.yanyi@gmail.com>
2014-12-30 00:38:39 +0800

    gunicorn

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import url_for
 from flask import render_template
 from flask import request
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.debug = True
@@ -9,6 +10,8 @@ app.debug = True
 @app.route("/")
 def index():
     return render_template('index.html')
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run()
